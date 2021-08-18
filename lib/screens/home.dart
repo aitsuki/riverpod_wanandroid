87830a46ad3f058448aa5292e9a9cfaa5b8ccb8a
aitsuki <aitsukigo@gmail.com>
2021-08-18 12:20:22 +0800

    fix: TabBar Controller's length property does not match...

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,7 +14,7 @@ class Home extends ConsumerWidget {
     final loadResult = ref.watch(loadHomeTabsProvider);
     return Scaffold(
       body: loadResult.when(
-        data: (_) => const _HomeContent(),
+        data: (_) => _HomeContent(),
         loading: () => const PageLoading(),
         error: (_, __) =>
             PageError(onReload: () => ref.refresh(loadHomeTabsProvider)),
@@ -23,20 +23,36 @@ class Home extends ConsumerWidget {
   }
 }
 
-class _HomeContent extends ConsumerWidget {
-  const _HomeContent({Key? key}) : super(key: key);
+class _HomeContent extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _HomeContentState();
+  }
+}
+
+class _HomeContentState extends ConsumerState<ConsumerStatefulWidget>
+    with TickerProviderStateMixin {
+  TabController? _tabController;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final homeTabs = ref.watch(homeTabNotifierProvider);
+    _tabController?.index = 0;
+    _tabController = TabController(length: homeTabs.length, vsync: this);
     return DefaultTabController(
       length: homeTabs.length,
       child: NestedScrollView(
-        // TODO(aitsuki): 这个功能会导致页面滚动初始化（下拉出现appbar时）
+        // TODO(aitsuki): 这个功能导致页面滚动初始化（下拉出现appbar时）
         // 如果不使用PageStorageKey而是AutomaticKeepAliveMixin，那么又会出现列表同步
         // 滚动的bug。试过extended_nested_scroll_view，并未解决列表初始化问题。
         // https://github.com/flutter/flutter/issues/40740
-        // floatHeaderSlivers: true,
+        floatHeaderSlivers: true,
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
             SliverOverlapAbsorber(
@@ -51,6 +67,7 @@ class _HomeContent extends ConsumerWidget {
                       onTap: () => Navigator.pushNamed(context, '/tab_setting'),
                       child: const Icon(Icons.add, color: Colors.white)),
                   tabBar: TabBar(
+                    controller: _tabController,
                     isScrollable: true,
                     indicatorSize: TabBarIndicatorSize.label,
                     tabs: homeTabs.map((tab) => Tab(text: tab.name)).toList(),
@@ -61,6 +78,7 @@ class _HomeContent extends ConsumerWidget {
           ];
         },
         body: TabBarView(
+          controller: _tabController,
           children: homeTabs.map((tab) {
             return SafeArea(
               top: false,
